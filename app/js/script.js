8d57bb2b6b4e5ae721c3e334e5ba30cbf42b0fb8
desi <desimichael@desis-MacBook-Pro.local>
2020-12-22 15:53:57 -0500

    Created mixins for media, animations and hero section.

--- app/js/script.js
+++ app/js/script.js
@@ -1,11 +1,28 @@
-const hamburgerBtn = document.querySelector("#hamburgerBtn");
+const hamburgerBtn = document.querySelector('#hamburgerBtn');
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElms = document.querySelectorAll('.has-fade');
 
-hamburgerBtn.addEventListener("click", function () {
-  console.log("open hamburger");
+header.addEventListener('click', function () {
+  console.log('click hamburger');
 
-  if (hamburgerBtn.classList.contains("open")) {
-    hamburgerBtn.classList.remove("open");
+  if (header.classList.contains('open')) {
+    // close hamburger menu
+    body.classList.remove('noscroll');
+    header.classList.remove('open');
+    fadeElms.forEach(function (element) {
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
+    });
+
   } else {
-    hamburgerBtn.classList.add('open');
+    // open hamburger menu
+    body.classList.add('noscroll');
+    header.classList.add('open');
+    fadeElms.forEach(function (element) {
+      element.classList.remove('fade-out');
+      element.classList.add('fade-in');
+    })
   }
 });
